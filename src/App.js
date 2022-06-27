26b81d53e403880814bf21ba59f2eef2dfeb2c3f
ManuelsSaNt <smaniegocaballo@gmail.com>
2022-06-27 02:04:01 -0600

    Summary

--- src/App.js
+++ src/App.js
@@ -59,7 +59,7 @@ function App() {
         symbol='BTC'
         foto='bitcoin-logo.png'
         altImage='Imagen Logo Bitcoin'
-        descrip='Bitcoin, creada el 31 de octubre de 2008 es una moneda digital  o cryptomoneda que permite pagar con un dispositivo movil desde su monedero o con un codigo QR, funciona en todas partes y en cualquier momento con pagos rapidos e internacionales si asi se desea pues se puede transferir Bitcoin de Africa a Canada en 10min no hay banco que atrase o trabaje en el proceso o honorarios que congelen la transeferencia, Bitcoin permite enviar y recibir pagos a casi coste cero. Salvo casos especiales, como en pagos diminutos, no existen tasas. sin emvargo puedes optar por pagar una tasa voluntaria para priorisar tu transaccion y remunerar a las personas que hacen funcionar la red de Bitcoin.' 
+        descrip='Bitcoin, created on October 31, 2008, is a digital currency or cryptocurrency that allows you to pay with a mobile device from your wallet or with a QR code, it works everywhere and at any time with fast and international payments if you wish. you can transfer Bitcoin from Africa to Canada in 10min there is no bank to delay or work in the process or fees that freeze the transfer, Bitcoin allows you to send and receive payments at almost zero cost. Except for special cases, such as tiny payments, there are no fees. however, you can choose to pay a voluntary fee to prioritize your transaction and pay the people who run the Bitcoin network.        ' 
         />
 
         <Cryptomoneda
@@ -67,7 +67,7 @@ function App() {
         symbol='ETH'
         foto='ether-logo.png'
         altImage='Imagen Logo Bitcoin'
-        descrip='Bitcoin, creada el 31 de octubre de 2008 es una moneda digital  o cryptomoneda que permite pagar con un dispositivo movil desde su monedero o con un codigo QR, funciona en todas partes y en cualquier momento con pagos rapidos e internacionales si asi se desea pues se puede transferir Bitcoin de Africa a Canada en 10min no hay banco que atrase o trabaje en el proceso o honorarios que congelen la transeferencia, Bitcoin permite enviar y recibir pagos a casi coste cero. Salvo casos especiales, como en pagos diminutos, no existen tasas. sin emvargo puedes optar por pagar una tasa voluntaria para priorisar tu transaccion y remunerar a las personas que hacen funcionar la red de Bitcoin.' 
+        descrip="Ethereum is a decentralized open source blockchain system that includes its own cryptocurrency, Ether. ETH functions as a platform for numerous other cryptocurrencies, as well as for the execution of decentralized smart contracts. Ethereum was first described in a 2013 whitepaper by Vitalik Buterin. Buterin, along with other co-founders, secured funding for the project in an online public sale in the summer of 2014 and officially launched the blockchain on July 30, 2015. Ethereum's goal is to become a global platform for decentralized applications, allowing users around the world to write and run software that is resistant to censorship, downtime, and fraud."
         />
 
         <Cryptomoneda 
@@ -75,7 +75,7 @@ function App() {
         moneda='Polkadot'
         foto='polka-logo.png'
         altImage='Imagen Logo Bitcoin'
-        descrip='pene del ranger esta pequeño'        
+        descrip="Polkadot is an open-source sharded multichain protocol that connects and secures a network of specialized blockchains, facilitating cross-chain transfer of any data or asset types, not just tokens, thereby allowing blockchains to be interoperable with each other. Polkadot was designed to provide a foundation for a decentralized internet of blockchains, also known as Web3. Polkadot is known as a layer-0 metaprotocol because it underlies and describes a format for a network of layer 1 blockchains known as parachains (parallel chains). As a metaprotocol, Polkadot is also capable of autonomously and forklessly updating its own codebase via on-chain governance according to the will of its token holder community. Polkadot provides a foundation to support a decentralized web, controlled by its users, and to simplify the creation of new applications, institutions and services. The Polkadot protocol can connect public and private chains, permissionless networks, oracles and future technologies, allowing these independent blockchains to trustlessly share information and transactions through the Polkadot Relay Chain (explained further down). Polkadot's native DOT token serves three clear purposes: staking for operations and security, facilitating network governance, and bonding tokens to connect parachains ."        
         />
 
         <Cryptomoneda
@@ -83,7 +83,7 @@ function App() {
         symbol='XRP'
         foto='ripple-logo.png'
         altImage='Imagen Logo Ripple'
-        descrip='pene del ranger esta pequeño'        
+        descrip='Launched in 2021, the XRP Ledger (XRPL) is an open-source, permissionless and decentralized technology. Benefits of the XRP Ledger include its low-cost ($0.0002 to transact), speed (settling transactions in 3-5 seconds), scalability (1,500 transactions per second) and inherently green attributes (carbon-neutral and energy-efficient). The XRP Ledger also features the first decentralized exchange (DEX) and custom tokenization capabilities built into the protocol. Since 2012, the XRP Ledger has been operating reliably, having closed 70 million ledgers.'        
         />
 
       </div>
@@ -107,8 +107,8 @@ function App() {
 
           <div className='lado'>
             <h5>Contact</h5>
-            <p><a  href='https://github.com/ManuelsSaNt' target='_blank'>Github</a></p>
-            <p><a  href='' target='_blank'>Developer</a></p>
+            <p><a  href='https://github.com/ManuelsSaNt/cryptos-practica' target='_blank'>Github</a></p>
+            <p><a  href='https://manuelssant.github.io/Presentacion/' target='_blank'>Developer</a></p>
           </div>
         </div>
 
